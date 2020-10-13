d5ddcdaba8ffe51004e09dab967255c12e6156bf
celer7841 <celer7841@wrdsb.ca>
2020-10-13 02:48:31 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841
  */
 public class Main {
 
@@ -10,6 +11,24 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner sc = new Scanner(System.in);
+    int[] numbers = new int [2]; 
+
+
+    System.out.println("Please enter the 2 values for the array");
+    numbers[0] = sc.nextInt();
+    numbers[1] = sc.nextInt();
     
+    
+    if(numbers[0] > numbers[1]){
+
+      int temp = numbers[0];
+      numbers[0] = numbers[1];
+      numbers[1] = temp;
+    }
+
+
+
+    System.out.println("Your numbers in ascending order are "+ numbers[0] + " and " + numbers[1]);
   }
 }
